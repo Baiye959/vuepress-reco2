c8bdc7160b2a776fdc1dd80100a98a38b787f3b6
Baiye959 <3383522774@qq.com>
2024-01-23 20:24:15 +0800

    change

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git pull
 cp -rf .vuepress/dist/ ~
-mv -f ~/dist ~/html
+rm -r ~/html
+mv ~/dist ~/html
 cp -rf ~/html /var/www
---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yarn install
+git add .
+git commit -m "change"
+git push
