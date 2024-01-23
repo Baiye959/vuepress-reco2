8b0deeeb032181698a81c516ffadc7fa338061bc
Baiye959 <3383522774@qq.com>
2024-01-24 00:49:36 +0800

    change

---
+++ .vuepress/dist/assets/index.html--G9ddTXP.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-2bee0752","path":"/categories/Linuxjichushiyongfangfa/1/","title":"","lang":"en-US","frontmatter":{"layout":"Categories"},"headers":[],"git":{},"filePathRelative":null}');export{e as data};
---
+++ blogs/linux/README.md
@@ -0,0 +1,8 @@
+---
+title: 一、准备学习环境
+date: 2024/01/24
+categories:
+ - Linux基础使用方法
+---
+## 使用vmware安装CentOS Linux 7
+[CentOS7镜像](https://mirrors.aliyun.com/centos/7/isos/x86_64/CentOS-7-x86_64-Minimal-2009.iso)
