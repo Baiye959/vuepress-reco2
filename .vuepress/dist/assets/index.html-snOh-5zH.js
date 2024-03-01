b2c396fcc7371e48ca2515bcb501de746f568cb7
Baiye959 <3383522774@qq.com>
2024-03-01 15:31:39 +0800

    change

--- .vuepress/dist/assets/index.html-snOh-5zH.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-62d4bc8b","path":"/categories/caozuoxitong/1/","title":"","lang":"en-US","frontmatter":{"layout":"Categories"},"headers":[],"git":{},"filePathRelative":null}');export{t as data};
--- .vuepress/dist/assets/leetcode206.html-JEm7Undj.js
+++
@@ -1,9 +0,0 @@
-import{_ as e,o as a,c as d,d as i}from"./app-4Gu1Q0GI.js";const n="/image/2024022901.jpg",s="/image/2024022902.jpg",t={},l=i('<h2 id="_206-反转链表" tabindex="-1"><a class="header-anchor" href="#_206-反转链表" aria-hidden="true">#</a> 206. 反转链表</h2><p>给你单链表的头节点 <code>head</code> ，请你反转链表，并返回反转后的链表。</p><p>示例 1： <img src="'+n+`" alt=""></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入：head = [1,2,3,4,5]
-输出：[5,4,3,2,1]
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>示例 2： <img src="`+s+`" alt=""></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入：head = [1,2]
-输出：[2,1]
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>示例 3：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入：head = []
-输出：[]
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>提示：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>链表中节点的数目范围是 [0, 5000]
--5000 &lt;= Node.val &lt;= 5000
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？</p>`,11),c=[l];function r(o,v){return a(),d("div",null,c)}const m=e(t,[["render",r],["__file","leetcode206.html.vue"]]);export{m as default};
