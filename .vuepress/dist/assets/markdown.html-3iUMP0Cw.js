d167bf709cefa5841e6f94aa404ff85796d2a120
baiye959 <3383522774@qq.com>
2024-03-06 20:38:28 +0800

    change

---
+++ .vuepress/dist/assets/markdown.html-3iUMP0Cw.js
@@ -0,0 +1,31 @@
+import{_ as t,r,o as d,c as l,a as e,b as n,d as s,e as i}from"./app-YJEJ9-8J.js";const c="/image/1706019725841.jpg",o="/image/dog.jpg",h={},u=e("h2",{id:"本主题markdown扩展",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#本主题markdown扩展","aria-hidden":"true"},"#"),n(" 本主题Markdown扩展")],-1),v=e("h3",{id:"自定义容器-代码相关",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#自定义容器-代码相关","aria-hidden":"true"},"#"),n(" 自定义容器&代码相关")],-1),m={href:"https://vuepress-theme-reco.recoluan.com/docs/theme/custom-container.html",target:"_blank",rel:"noopener noreferrer"},b=i(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>::: tip
+这是一个提示
+:::
+
+::: info
+这是一个提示
+:::
+
+::: warning
+这是一个警告
+:::
+
+::: danger
+这是一个危险警告
+:::
+
+::: details
+这是一个 details 标签
+:::
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="任务列表" tabindex="-1"><a class="header-anchor" href="#任务列表" aria-hidden="true">#</a> 任务列表</h3>`,2),p={href:"https://vuepress-theme-reco.recoluan.com/docs/theme/markdown-task.html",target:"_blank",rel:"noopener noreferrer"},g=i(`<ul class="contains-task-list"><li class="task-list-item"><input class="task-list-item-checkbox" disabled="" type="checkbox"> 未完成任务</li><li class="task-list-item"><input class="task-list-item-checkbox" checked="" disabled="" type="checkbox"> 已完成任务</li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>- [ ] 未完成任务
+- [x] 已完成任务
+ * * *注意这些空格必须要有
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="vue组件预览" tabindex="-1"><a class="header-anchor" href="#vue组件预览" aria-hidden="true">#</a> Vue组件预览</h3>`,3),_={href:"https://vuepress-theme-reco.recoluan.com/docs/theme/custom-container.html",target:"_blank",rel:"noopener noreferrer"},x=e("h3",{id:"解析文件代码组",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#解析文件代码组","aria-hidden":"true"},"#"),n(" 解析文件代码组")],-1),f={href:"https://vuepress-theme-reco.recoluan.com/docs/theme/markdown-file-parse.html",target:"_blank",rel:"noopener noreferrer"},k=e("br",null,null,-1),w=e("h2",{id:"常用markdown语法",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#常用markdown语法","aria-hidden":"true"},"#"),n(" 常用Markdown语法")],-1),y={href:"https://markdown.com.cn/basic-syntax/",target:"_blank",rel:"noopener noreferrer"},j=i('<h3 id="标题" tabindex="-1"><a class="header-anchor" href="#标题" aria-hidden="true">#</a> 标题</h3><p>文内标题只能从##开始！#被文章标题占用</p><h3 id="换行" tabindex="-1"><a class="header-anchor" href="#换行" aria-hidden="true">#</a> 换行</h3><p><img src="'+c+'" alt=""></p><h3 id="插入图片-包括svg图标" tabindex="-1"><a class="header-anchor" href="#插入图片-包括svg图标" aria-hidden="true">#</a> 插入图片（包括svg图标）</h3><p><img src="'+o+`" alt=""> 该图片路径为.vuepress/public/image/dog.jpg</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>![](/image/1706019725841.jpg)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h3 id="插入文件-包括视频" tabindex="-1"><a class="header-anchor" href="#插入文件-包括视频" aria-hidden="true">#</a> 插入文件（包括视频）</h3><p><a href="/file/ratings_wfpL8zQ8L4.csv">ratings.csv</a></p><p>该文件路径为.vuepress/public/file/ratings_wfpL8zQ8L4.csv，希望展示的名称为ratings.csv</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>[ratings.csv](/file/ratings_wfpL8zQ8L4.csv)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h3 id="表格" tabindex="-1"><a class="header-anchor" href="#表格" aria-hidden="true">#</a> 表格</h3>`,12),L=e("thead",null,[e("tr",null,[e("th",{style:{"text-align":"left"}},"左对齐"),e("th",{style:{"text-align":"center"}},"居中对齐"),e("th",{style:{"text-align":"right"}},"右对齐")])],-1),V=e("td",{style:{"text-align":"left"}},"Chocolatey",-1),N=e("td",{style:{"text-align":"center"}},"Windows 命令行软件管理器",-1),z={style:{"text-align":"right"}},B={href:"https://javabetter.cn/gongju/choco.html",target:"_blank",rel:"noopener noreferrer"},C=i(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>| 左对齐 | 居中对齐 | 右对齐 |
+|:-|:-:|-:|
+| Chocolatey | Windows 命令行软件管理器 | [使用教程](https://javabetter.cn/gongju/choco.html) |
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="引用" tabindex="-1"><a class="header-anchor" href="#引用" aria-hidden="true">#</a> 引用</h3><blockquote><p>引用第一行</p><p>引用第二行</p></blockquote><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&gt;引用第一行
+&gt;
+&gt;引用第二行
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4);function E(M,Q){const a=r("ExternalLinkIcon");return d(),l("div",null,[u,v,e("p",null,[e("a",m,[n("官网文档 - 自定义容器&代码相关"),s(a)])]),b,e("p",null,[e("a",p,[n("官网文档 - 任务列表"),s(a)])]),g,e("p",null,[e("a",_,[n("官网文档 - Vue组件预览"),s(a)])]),x,e("p",null,[e("a",f,[n("官网文档 - 解析文件代码组"),s(a)]),k]),w,e("p",null,[e("a",y,[n("Markdown官方语法"),s(a)])]),j,e("table",null,[L,e("tbody",null,[e("tr",null,[V,N,e("td",z,[e("a",B,[n("使用教程"),s(a)])])])])]),C])}const I=t(h,[["render",E],["__file","markdown.html.vue"]]);export{I as default};
