2e49d021c2f4d4175048bad929d467674ab8b4e9
baiye959 <3383522774@qq.com>
2024-03-09 13:10:52 +0800

    change

---
+++ .vuepress/dist/assets/leetcode206.html-hVmkm4bB.js
@@ -0,0 +1,52 @@
+import{_ as n,o as s,c as a,e}from"./app-ekpjnQKr.js";const t="/image/2024022901.jpg",i="/image/2024022902.jpg",p={},c=e('<h2 id="_206-反转链表" tabindex="-1"><a class="header-anchor" href="#_206-反转链表" aria-hidden="true">#</a> 206. 反转链表</h2><p>给你单链表的头节点 <code>head</code> ，请你反转链表，并返回反转后的链表。</p><p>示例 1： <img src="'+t+`" alt=""></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>示例 2： <img src="`+i+`" alt=""></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入：head = [1,2]
+输出：[2,1]
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>示例 3：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入：head = []
+输出：[]
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>提示：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>链表中节点的数目范围是 [0, 5000]
+-5000 &lt;= Node.val &lt;= 5000
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？</p><h2 id="解题思路" tabindex="-1"><a class="header-anchor" href="#解题思路" aria-hidden="true">#</a> 解题思路</h2><h3 id="解法一-双指针" tabindex="-1"><a class="header-anchor" href="#解法一-双指针" aria-hidden="true">#</a> 解法一（双指针）</h3><p>使用双指针，指针<code>cur</code>指向当前节点、指针<code>pre</code>指向上一节点，方便指针翻转。<br> 注意需要在每次循环开始时，记录下一节点，不然指针翻转后会无法找到下一节点，可以用指针<code>temp</code>存储。<br> 终止条件：无当前节点，即指针<code>cur</code>为空。</p><div class="language-java line-numbers-mode" data-ext="java"><pre class="language-java"><code><span class="token doc-comment comment">/**
+ * Definition for singly-linked list.
+ * public class ListNode <span class="token punctuation">{</span>
+ *     int val;
+ *     ListNode next;
+ *     ListNode() <span class="token punctuation">{</span><span class="token punctuation">}</span>
+ *     ListNode(int val) <span class="token punctuation">{</span> this.val = val; <span class="token punctuation">}</span>
+ *     ListNode(int val, ListNode next) <span class="token punctuation">{</span> this.val = val; this.next = next; <span class="token punctuation">}</span>
+ * <span class="token punctuation">}</span>
+ */</span>
+<span class="token keyword">class</span> <span class="token class-name">Solution</span> <span class="token punctuation">{</span>
+    <span class="token keyword">public</span> <span class="token class-name">ListNode</span> <span class="token function">reverseList</span><span class="token punctuation">(</span><span class="token class-name">ListNode</span> head<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token class-name">ListNode</span> cur <span class="token operator">=</span> head<span class="token punctuation">,</span> pre <span class="token operator">=</span> <span class="token keyword">null</span><span class="token punctuation">,</span> temp<span class="token punctuation">;</span>
+        <span class="token keyword">while</span> <span class="token punctuation">(</span>cur <span class="token operator">!=</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+            temp <span class="token operator">=</span> cur<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+            cur<span class="token punctuation">.</span>next <span class="token operator">=</span> pre<span class="token punctuation">;</span>
+            pre <span class="token operator">=</span> cur<span class="token punctuation">;</span>
+            cur <span class="token operator">=</span> temp<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span>
+        <span class="token keyword">return</span> pre<span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="解法二-递归" tabindex="-1"><a class="header-anchor" href="#解法二-递归" aria-hidden="true">#</a> 解法二（递归）</h3><p>递归的处理过程实际上跟双指针解法一致，但是思路比双指针更难理解一些。<br> 实际上，递归就是将每次的指针翻转包装成一个函数，然后在函数的最后进入下一次的指针翻转。<br> 函数退出的条件同样是指针<code>cur</code>为空。</p><div class="language-java line-numbers-mode" data-ext="java"><pre class="language-java"><code><span class="token doc-comment comment">/**
+ * Definition for singly-linked list.
+ * public class ListNode <span class="token punctuation">{</span>
+ *     int val;
+ *     ListNode next;
+ *     ListNode() <span class="token punctuation">{</span><span class="token punctuation">}</span>
+ *     ListNode(int val) <span class="token punctuation">{</span> this.val = val; <span class="token punctuation">}</span>
+ *     ListNode(int val, ListNode next) <span class="token punctuation">{</span> this.val = val; this.next = next; <span class="token punctuation">}</span>
+ * <span class="token punctuation">}</span>
+ */</span>
+<span class="token keyword">class</span> <span class="token class-name">Solution</span> <span class="token punctuation">{</span>
+    <span class="token keyword">public</span> <span class="token class-name">ListNode</span> <span class="token function">oneReverse</span><span class="token punctuation">(</span><span class="token class-name">ListNode</span> cur<span class="token punctuation">,</span> <span class="token class-name">ListNode</span> pre<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">if</span> <span class="token punctuation">(</span>cur <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token keyword">return</span> pre<span class="token punctuation">;</span>
+        <span class="token class-name">ListNode</span> temp <span class="token operator">=</span> cur<span class="token punctuation">.</span>next<span class="token punctuation">;</span>
+        cur<span class="token punctuation">.</span>next <span class="token operator">=</span> pre<span class="token punctuation">;</span>
+        <span class="token keyword">return</span> <span class="token function">oneReverse</span><span class="token punctuation">(</span>temp<span class="token punctuation">,</span> cur<span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token keyword">public</span> <span class="token class-name">ListNode</span> <span class="token function">reverseList</span><span class="token punctuation">(</span><span class="token class-name">ListNode</span> head<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">return</span> <span class="token function">oneReverse</span><span class="token punctuation">(</span>head<span class="token punctuation">,</span> <span class="token keyword">null</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,18),l=[c];function o(d,u){return s(),a("div",null,l)}const v=n(p,[["render",o],["__file","leetcode206.html.vue"]]);export{v as default};
--- blogs/algorithm/acm_io.md
+++ blogs/algorithm/acm_io.md
@@ -11,7 +11,7 @@ categories:
 :::
 
 
-::: details 第1题——A+B问题I
+::: details 1.多行输入，每行两个整数
 ## 1. A+B问题I
 ### 题目
 时间限制：2.000S  空间限制：32MB
@@ -57,7 +57,7 @@ public class Main {
 ```
 :::
 
-::: details 第2题——A+B问题II
+::: details 2.多组数据，每组第一行为n，之后输入n行两个整数
 ## 2. A+B问题II
 ### 题目
 时间限制：1.000S  空间限制：32MB
@@ -109,7 +109,7 @@ public class Main {
 ```
 :::
 
-::: details 第3题——A+B问题III
+::: details 3.若干行输入，每行输入两个整数，遇到特定条件终止
 ## 3. A+B问题III
 ### 题目
 时间限制：1.000S  空间限制：32MB
@@ -157,7 +157,7 @@ public class Main {
 ```
 :::
 
-::: details 第4题——A+B问题IV
+::: details 4.若干行输入，遇到0终止，每行第一个数为N，表示本行后面有N个数
 ## 4. A+B问题IV
 ### 题目
 时间限制：1.000S  空间限制：32MB
@@ -208,7 +208,7 @@ public class Main {
 ```
 :::
 
-::: details 第5题——A+B问题VII
+::: details 5.若干行输入，每行包括两个整数a和b，由空格分隔，每行输出后接一个空行
 ## 5. A+B问题VII
 ### 题目
 时间限制：1.000S  空间限制：32MB
@@ -254,3 +254,192 @@ public class Main {
 ```
 :::
 
+::: details 6.多组行数据，每行先输入一个整数N，然后在同一行内输入M和M个整数，每组输出之间输出一个空行
+## 6. A+B问题VIII
+### 题目
+时间限制：1.000S  空间限制：32MB
+
+题目描述
+> 你的任务是计算若干整数的和。
+
+输入描述
+> 输入的第一行为一个整数N，接下来N行每行先输入一个整数M，然后在同一行内输入M个整数。
+
+输出描述
+> 对于每组输入，输出M个数的和，每组输出之间输出一个空行。
+
+输入示例
+```
+3
+4 1 2 3 4
+5 1 2 3 4 5
+3 1 2 3
+```
+输出示例
+```
+10
+
+15
+
+6
+```
+
+提示信息
+```
+注意以上样例为一组测试数据，后端判题会有很多组测试数据，也就是会有多个N的输入
+例如输入可以是：
+3
+4 1 2 3 4
+5 1 2 3 4 5
+3 1 2 3
+3
+4 1 2 3 4
+5 1 2 3 4 5
+3 1 2 3
+输出则是
+10
+
+15
+
+6
+10
+
+15
+
+6
+只保证每组数据间是有空行的。但两组数据并没有空行
+```
+
+### 解题
+```java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            int n = sc.nextInt();
+            for (int i=0; i<n; i++) {
+                int m = sc.nextInt();
+                int sum = 0;
+                for (int j=0; j<m; j++) {
+                    sum += sc.nextInt();
+                }
+                System.out.println(sum);
+                if (i != n - 1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
+```
+:::
+
+::: details 7.多组测试样例，每组输入数据为字符串，字符用空格分隔，输出为小数点后两位
+## 7. 平均绩点
+### 题目
+时间限制：1.000S  空间限制：32MB
+
+题目描述
+> 每门课的成绩分为A、B、C、D、F五个等级，为了计算平均绩点，规定A、B、C、D、F分别代表4分、3分、2分、1分、0分。
+
+输入描述
+> 有多组测试样例。每组输入数据占一行，由一个或多个大写字母组成，字母之间由空格分隔。
+
+输出描述
+> 每组输出结果占一行。如果输入的大写字母都在集合｛A,B,C,D,F｝中，则输出对应的平均绩点，结果保留两位小数。否则，输出"Unknown"。
+
+输入示例
+```
+A B C D F
+B F F C C A
+D C E F
+```
+
+输出示例
+```
+2.00
+1.83
+Unknown
+```
+
+### 解题
+注意这里第五个等级是F不是E...
+```java
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] items = line.split(" ");
+            
+            double sum = 0.0;
+            boolean hasUnknown = false;
+            for (String item : items) {
+                switch (item) {
+                    case "A":
+                        sum += 4; continue;
+                    case "B":
+                        sum += 3; continue;
+                    case "C":
+                        sum += 2; continue;
+                    case "D":
+                        sum += 1; continue;
+                    case "F":
+                        continue;
+                    default:
+                        hasUnknown = true; break;
+                }
+            }
+            
+            if (hasUnknown) {
+                System.out.println("Unknown");
+            } else {
+                System.out.println(String.format("%.2f", sum / items.length));
+            }
+        }
+    }
+}
+```
+:::
+
+::: details 8.多组测试用例，第一行为正整数n，第二行为n个正整数，n=0时，结束输入，每组输出结果的下面都输出一个空行
+## 8. 摆平积木
+### 题目
+时间限制：1.000S  空间限制：32MB
+
+题目描述
+> 小明很喜欢玩积木。一天，他把许多积木块组成了好多高度不同的堆，每一堆都是一个摞一个的形式。然而此时，他又想把这些积木堆变成高度相同的。但是他很懒，他想移动最少的积木块来实现这一目标，你能帮助他吗？
+> ![](/image/2024030901.png)
+
+输入描述
+> 输入包含多组测试样例。每组测试样例包含一个正整数n，表示小明已经堆好的积木堆的个数。<br/>
+> 接着下一行是n个正整数，表示每一个积木堆的高度h，每块积木高度为1。其中1<=n<=50,1<=h<=100。<br/>
+> 测试数据保证积木总数能被积木堆数整除。<br/>
+> 当n=0时，输入结束。
+
+输出描述
+> 对于每一组数据，输出将积木堆变成相同高度需要移动的最少积木块的数量。<br/>
+> 在每组输出结果的下面都输出一个空行。
+
+输入示例
+```
+6
+5 2 4 1 7 5
+0
+```
+
+输出示例
+```
+5
+```
+
+### 解题
+```java
+111
+```
+:::
