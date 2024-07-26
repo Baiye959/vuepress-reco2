5e62d3632dc21f1bd967fd239f193123f4cf0aab
baiye959 <3383522774@qq.com>
2024-07-26 15:56:05 +0800

    change

---
+++ .vuepress/dist/assets/leetcode450.html-fZoHH67k.js
@@ -0,0 +1,55 @@
+import{_ as n,o as s,c as a,e}from"./app-TQwmEIwM.js";const t="/image/2024032409.jpg",l="/image/2024032410.jpg",o="/image/2024032501.png",p="/image/2024032502.png",i={},c=e('<h2 id="_450-删除二叉搜索树中的节点" tabindex="-1"><a class="header-anchor" href="#_450-删除二叉搜索树中的节点" aria-hidden="true">#</a> 450. 删除二叉搜索树中的节点</h2><p>给定一个二叉搜索树的根节点 <strong>root</strong> 和一个值 <strong>key</strong>，删除二叉搜索树中的 <strong>key</strong> 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。</p><p>一般来说，删除节点可分为两个步骤：</p><ol><li>首先找到需要删除的节点；</li><li>如果找到了，删除它。</li></ol><p>示例 1: <img src="'+t+`" alt=""></p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入：root = [5,3,6,2,4,null,7], key = 3
+输出：[5,4,6,2,null,null,7]
+解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+另一个正确答案是 [5,2,6,null,4,null,7]。
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="`+l+`" alt=""></p><p>示例 2:</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入: root = [5,3,6,2,4,null,7], key = 0
+输出: [5,3,6,2,4,null,7]
+解释: 二叉树不包含值为 0 的节点
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>示例 3:</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>输入: root = [], key = 0
+输出: []
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><p>提示:</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>节点数的范围 [0, 10^4].
+-10^5 &lt;= Node.val &lt;= 10^5
+节点值唯一
+root 是合法的二叉搜索树
+-10^5 &lt;= key &lt;= 10^5
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>进阶： 要求算法时间复杂度为 <code>O(h)</code>，<code>h</code> 为树的高度。</p><h2 id="解题思路" tabindex="-1"><a class="header-anchor" href="#解题思路" aria-hidden="true">#</a> 解题思路</h2><p>在树中如何删除当前节点？<br> 递归函数<code>f</code>的返回值为节点，那么在处理当前节点<code>cur</code>时，执行<strong>cur.left = f(cur.left, ...)</strong>，<code>cur.left</code>就可以在自己那层递归 通过<code>return</code>其他的节点 将自己删除。</p><p>考虑删除时的几种情况：</p><ol><li>删除节点不存在，直接返回null</li><li>删除节点无左子树和右子树，直接返回null</li><li>删除节点无左子树，返回右子树</li><li>删除节点无右子树，返回左子树</li><li>删除节点左右子树都在，将左子树变成右子树最小节点的左孩子（最小节点是一路left）</li></ol><p>第五种情况如图： <img src="`+o+'" alt=""><img src="'+p+`" alt=""></p><div class="language-java line-numbers-mode" data-ext="java"><pre class="language-java"><code><span class="token doc-comment comment">/**
+ * Definition for a binary tree node.
+ * public class TreeNode <span class="token punctuation">{</span>
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() <span class="token punctuation">{</span><span class="token punctuation">}</span>
+ *     TreeNode(int val) <span class="token punctuation">{</span> this.val = val; <span class="token punctuation">}</span>
+ *     TreeNode(int val, TreeNode left, TreeNode right) <span class="token punctuation">{</span>
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     <span class="token punctuation">}</span>
+ * <span class="token punctuation">}</span>
+ */</span>
+<span class="token keyword">class</span> <span class="token class-name">Solution</span> <span class="token punctuation">{</span>
+    <span class="token keyword">public</span> <span class="token class-name">TreeNode</span> <span class="token function">deleteNode</span><span class="token punctuation">(</span><span class="token class-name">TreeNode</span> root<span class="token punctuation">,</span> <span class="token keyword">int</span> key<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+        <span class="token keyword">if</span> <span class="token punctuation">(</span>root <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token keyword">return</span> <span class="token keyword">null</span><span class="token punctuation">;</span>
+        <span class="token keyword">if</span> <span class="token punctuation">(</span>root<span class="token punctuation">.</span>val <span class="token operator">==</span> key<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+            <span class="token keyword">if</span> <span class="token punctuation">(</span>root<span class="token punctuation">.</span>left <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+                <span class="token keyword">return</span> root<span class="token punctuation">.</span>right<span class="token punctuation">;</span>
+            <span class="token punctuation">}</span> <span class="token keyword">else</span> <span class="token keyword">if</span> <span class="token punctuation">(</span>root<span class="token punctuation">.</span>right <span class="token operator">==</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+                <span class="token keyword">return</span> root<span class="token punctuation">.</span>left<span class="token punctuation">;</span>
+            <span class="token punctuation">}</span> <span class="token keyword">else</span> <span class="token punctuation">{</span>
+                <span class="token class-name">TreeNode</span> cur <span class="token operator">=</span> root<span class="token punctuation">.</span>right<span class="token punctuation">;</span>
+                <span class="token keyword">while</span> <span class="token punctuation">(</span>cur<span class="token punctuation">.</span>left <span class="token operator">!=</span> <span class="token keyword">null</span><span class="token punctuation">)</span> <span class="token punctuation">{</span>
+                    cur <span class="token operator">=</span> cur<span class="token punctuation">.</span>left<span class="token punctuation">;</span>
+                <span class="token punctuation">}</span>
+                cur<span class="token punctuation">.</span>left <span class="token operator">=</span> root<span class="token punctuation">.</span>left<span class="token punctuation">;</span>
+                <span class="token keyword">return</span> root<span class="token punctuation">.</span>right<span class="token punctuation">;</span>
+            <span class="token punctuation">}</span>
+        <span class="token punctuation">}</span> <span class="token keyword">else</span> <span class="token keyword">if</span> <span class="token punctuation">(</span>key <span class="token operator">&lt;</span> root<span class="token punctuation">.</span>val<span class="token punctuation">)</span> <span class="token punctuation">{</span>
+            root<span class="token punctuation">.</span>left <span class="token operator">=</span> <span class="token function">deleteNode</span><span class="token punctuation">(</span>root<span class="token punctuation">.</span>left<span class="token punctuation">,</span> key<span class="token punctuation">)</span><span class="token punctuation">;</span>
+        <span class="token punctuation">}</span> <span class="token keyword">else</span> <span class="token punctuation">{</span>
+            root<span class="token punctuation">.</span>right <span class="token operator">=</span> <span class="token function">deleteNode</span><span class="token punctuation">(</span>root<span class="token punctuation">.</span>right<span class="token punctuation">,</span> key<span class="token punctuation">)</span><span class="token punctuation">;</span>
+        <span class="token punctuation">}</span>
+        <span class="token keyword">return</span> root<span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,20),u=[c];function r(d,k){return s(),a("div",null,u)}const m=n(i,[["render",r],["__file","leetcode450.html.vue"]]);export{m as default};
---
+++ blogs/other/rCore_0.md
@@ -0,0 +1,21 @@
+---
+title: rCore | 第零章：操作系统概述
+date: 2024/07/26
+categories:
+ - other
+tags:
+ - rCore
+---
+::: info
+[rCore-Tutorial-B0ok-v33.6.0-alpha.1文档](https://deathwish5.github.io/rCore-Tutorial-Book-v3/chapter0/index.html)
+:::
+
+选择使用VMware虚拟机种使用虚拟磁盘文件的方式进行本地OS开发环境配置。
+
+> 如果你打算使用 VMware 安装虚拟机的话，我们已经配置好了一个能直接运行 rCore-Tutorial-v3 的 Ubuntu18.04 镜像，它是一个 vmdk 格式的虚拟磁盘文件，只需要在 VMware 中新建一台虚拟机，在设置虚拟磁盘的时候选择它即可。[百度网盘链接](https://pan.baidu.com/s/1JzKjWivy9GZKK8rc3WMJ0g "百度网盘链接") （提取码 x5mf ）或者 [清华云盘链接](https://cloud.tsinghua.edu.cn/d/a9b7b0a1b4724c3f9c66/ "清华云盘链接") 。已经创建好用户 oslab ，密码为一个空格。它已经安装了中文输入法和 Markdown 编辑器 Typora 还有作为 Rust 集成开发环境的 Visual Studio Code，能够更容易完成实验并撰写实验报告。如果想要使用 VMWare 安装 openEuler 虚拟机的话，可以在 [openEuler官网](https://repo.openeuler.org/openEuler-20.03-LTS-SP2/ISO/ "openEuler官网") 下载 ISO 自行安装，接着需要参考网络上的一些教程配置网络和安装图形界面。
+>
+> 注：目前的虚拟机镜像基于ubuntu-18.04和QEMU-5.0，还没更新到ubuntu-20.04和QEMU-7.0
+
+使用虚拟磁盘文件新建虚拟机：[\[图文\]VMWARE虚拟机如何打开.VMDK格式的磁盘映像 – 蓝点网 (landiannews.com)](https://www.landiannews.com/archives/51945.html "\[图文]VMWARE虚拟机如何打开.VMDK格式的磁盘映像 – 蓝点网 (landiannews.com)")
+
+虚拟机无网络问题：[https://www.cnblogs.com/startnight/articles/13569831.html](https://www.cnblogs.com/startnight/articles/13569831.html "https://www.cnblogs.com/startnight/articles/13569831.html")
